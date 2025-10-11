0866a6c9364b348bce9d01760d4a5368bb2e726c
Suki <t.sukithansuki@gmail.com>
2025-10-11 14:37:49 +0530

    chore: update Jest configuration for verbose test output

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,12 @@
 import nextJest from "next/jest.js";
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files
-  dir: "./",
+  dir: "./", // path to your Next.js app
 });
 
-// Add any custom config to be passed to Jest
 const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleNameMapper: {
-    // Handle module aliases (this will be automatically configured for you based on your tsconfig.json paths)
     "^@/(.*)$": "<rootDir>/$1",
   },
   testEnvironment: "jest-environment-jsdom",
@@ -17,5 +14,4 @@ const customJestConfig = {
   transformIgnorePatterns: ["node_modules/(?!(bson|mongodb)/)"],
 };
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 export default createJestConfig(customJestConfig);
